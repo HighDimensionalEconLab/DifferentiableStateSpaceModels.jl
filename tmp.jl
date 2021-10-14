8423c557311ad9b896ef75566bd098179833276e
Peifan Wu <wupeifan@gmail.com>
2021-10-14 01:45:01 -0700

    experiment success

--- tmp.jl
+++ tmp.jl
@@ -24,13 +24,20 @@ end
 
 
 function ChainRulesCore.rrule(::typeof(f), p, p_f)
+    println("using custom rrule")
     val = f(p, p_f)
     # Could go through the keys(p) and fill in details for the derivatives here based on what is in it?
-
+    all_p = merge(p, p_f)
+    loadings = [(a = 2 * all_p.a, b = 1, c = 0, d = 0), (a = 0, b = 0, c = 2 * all_p.c, d = 2 * all_p.d)]
     function f_pb(Δsol)
-        Δp = # NEED TO FILL IN GIVEN THE APPROPRIATE KEY IN ORDER
-        return NoTangent(), Δp, NoTangent() 
+        println("using custom pb")
+        Δp = [] # NEED TO FILL IN GIVEN THE APPROPRIATE KEY IN ORDER
+        for key in keys(p)
+            push!(Δp, loadings[1][key] * Δsol[1] + loadings[2][key] * Δsol[2])
+        end
+        return NoTangent(), Tangent{typeof(p)}(; zip(keys(p), Δp)...), NoTangent() 
     end
+    return val, f_pb
 end
 
 # The example down there works
@@ -38,7 +45,18 @@ f(nt) = nt.a + 2 * nt.b
 
 function ChainRulesCore.rrule(::typeof(f), nt::NamedTuple)
     y = f(nt)
-    f_pullback(dy) = NoTangent(), Tangent{typeof(nt)}(a=dy, b=3*dy)
+    # assume some cache passed in here
+    tmp = (a = 1, b = 4)
+    function f_pullback(dy)
+        res_values = []
+        for key in keys(nt)
+            push!(res_values, tmp[key] * dy)
+        end
+        # res = NamedTuple{keys(nt)}(res_values)
+        # return NoTangent(), Tangent{typeof(nt)}(;res...)
+        return NoTangent(), Tangent{typeof(nt)}(; zip(keys(nt), res_values)...)
+    end
+    # f_pullback(dy) = NoTangent(), Tangent{typeof(nt)}(a=dy, b=3*dy)
     
     return y, f_pullback
 end
