b7c08c300d9b43374a21f559441049102b95c946
Peifan Wu <wupeifan@gmail.com>
2021-10-28 14:54:25 -0700

    Update rbc_estimation.jl

--- test/rbc_estimation.jl
+++ test/rbc_estimation.jl
@@ -1,37 +1,33 @@
-using DifferentiableStateSpaceModels,
-    LinearAlgebra,
-    Optim,
-    Test,
-    TimerOutputs,
-    Turing,
-    Zygote,
-    BenchmarkTools
+using DifferentiableStateSpaceModels, LinearAlgebra, Optim, Test, Turing, Zygote
+using ChainRulesCore
+using DifferentiableStateSpaceModels.Examples
+using Turing: @addlogprob!
 
 Turing.setadbackend(:zygote)
-using Turing: @addlogprob!
+
 # Create models from modules and then solve
 #p_d = [α, β]
-model_rbc = @include_example_module(Examples.rbc_observables_benchmark)
-model_rbc_second = @include_example_module(Examples.rbc_observables_benchmark, 2)
+model_rbc = @include_example_module(Examples.rbc_observables)
 
-p_f = [0.7, 0.1, 0.01, sqrt(0.0001)]
-# Generate artificial data
-p = [0.4, 0.96]
-sol = generate_perturbation(model_rbc, p; p_f)
-sol_second = generate_perturbation(model_rbc_second, p; p_f)
+p_f = (ρ=0.2, δ=0.02, σ=0.01, Ω_1=0.01)
+p_d = (α=0.5, β=0.95)
+p_d_input = [0.5, 0.95]
+sol = generate_perturbation(model_rbc, p_d, p_f, Val(1))
+sol_second = generate_perturbation(model_rbc, p_d, p_f, Val(2))
+
 T = 20
 ϵ = [randn(model_rbc.n_ϵ) for _ = 1:T]
 x0 = zeros(model_rbc.n_x)
-fake_z = solve(sol, x0, (0, T), LTI(); noise = ϵ).z
-fake_z_second = solve(sol_second, x0, (0, T), QTI(); noise = ϵ).z
+fake_z = solve(sol, x0, (0, T), DifferentiableStateSpaceModels.LTI(); noise = ϵ).z
+fake_z_second = solve(sol_second, x0, (0, T), DifferentiableStateSpaceModels.QTI(); noise = ϵ).z
 
-## Turing model, Kalman filter
+# Turing model, Kalman filter
 @model function rbc_kalman(z, m, p_f, cache)
     α ~ Uniform(0.2, 0.8)
     β ~ Uniform(0.5, 0.99)
-    p = [α, β]
-    sol = generate_perturbation(m, p; p_f, cache, settings = PerturbationSolverSettings(;
-    print_level = 1))
+    p_d = (α = α, β = β)
+    # println(p_d)
+    sol = generate_perturbation(m, p_d, p_f, Val(1); cache)
     if !(sol.retcode == :Success)
         @addlogprob! -Inf
         return
@@ -39,23 +35,43 @@ fake_z_second = solve(sol_second, x0, (0, T), QTI(); noise = ϵ).z
     @addlogprob! solve(sol, sol.x_ergodic, (0, length(z)); observables = z).logpdf
 end
 
-turing_model = rbc_kalman(fake_z, model_rbc, p_f, allocate_cache(model_rbc))
+c = SolverCache(model_rbc, Val(1), p_d)
+turing_model = rbc_kalman(fake_z, model_rbc, p_f, c)
 n_samples = 20
 n_adapts = 5
 δ = 0.65
 chain = sample(turing_model, NUTS(n_adapts, δ), n_samples; progress = true)
 θ_MAP = optimize(turing_model, MAP())
 
+# NOTE: a function wrap doesn't really help. It's a more general Zygote issue.
+# function generate_perturbation_wrap(m, p_d_input, p_f, order::Val{1}; cache)
+#     p_d = (α = p_d_input[1], β = p_d_input[2])
+#     sol = generate_perturbation(m, p_d, p_f, order; cache)
+#     return sol
+# end
+
+# function ChainRulesCore.rrule(::typeof(generate_perturbation_wrap), m, p_d_input, p_f, order::Val{1}; cache)
+#     println("using wrap 1")
+#     p_d = (α = p_d_input[1], β = p_d_input[2])
+#     sol, pb = ChainRulesCore.rrule(generate_perturbation, m, p_d, p_f, order; cache)
+#     function wrap_pb(Δsol)
+#         println("using wrap 2")
+#         res = pb(Δsol)
+#         return res[1], res[2], reduce(vcat, res[3]), res[4], res[5]
+#     end
+#     return sol, wrap_pb
+# end
+
 ## Turing model, Joint likelihood
 @model function rbc_joint(z, m, p_f, cache, x0 = zeros(m.n_x))
     α ~ Uniform(0.2, 0.8)
     β ~ Uniform(0.5, 0.99)
-    p = [α, β]
+    p_d = (α = α, β = β)
     T = length(z)
     ϵ_draw ~ MvNormal(T, 1.0)
     ϵ = map(i -> ϵ_draw[((i-1)*m.n_ϵ+1):(i*m.n_ϵ)], 1:T)
-    # println(p)
-    sol = generate_perturbation(m, p; p_f, cache)
+    # println(p_d)
+    sol = generate_perturbation(m, p_d, p_f, Val(1); cache)
     if !(sol.retcode == :Success)
         @addlogprob! -Inf
         return
@@ -63,7 +79,8 @@ chain = sample(turing_model, NUTS(n_adapts, δ), n_samples; progress = true)
     @addlogprob! solve(sol, x0, (0, T); noise = ϵ, observables = z).logpdf
 end
 
-turing_model = rbc_joint(fake_z, model_rbc, p_f, allocate_cache(model_rbc))
+c = SolverCache(model_rbc, Val(1), p_d)
+turing_model = rbc_joint(fake_z, model_rbc, p_f, c)
 n_samples = 20
 n_adapts = 3
 δ = 0.65
@@ -90,24 +107,22 @@ g = gradient(density, [p;vcat(ϵ...)])
 @model function rbc_second(z, m, p_f, cache, x0 = zeros(m.n_x))
     α ~ Uniform(0.2, 0.8)
     β ~ Uniform(0.5, 0.99)
-    p = [α, β]
+    p_d = (α = α, β = β)
     T = length(z)
     ϵ_draw ~ MvNormal(T, 1.0)
     ϵ = map(i -> ϵ_draw[((i-1)*m.n_ϵ+1):(i*m.n_ϵ)], 1:T)
-    # println(p)
-    # settings = PerturbationSolverSettings(; print_level = 2)
-    sol = generate_perturbation(m, p; p_f, cache)
+    # println(p_d)
+    sol = generate_perturbation(m, p_d, p_f, Val(2); cache)
     if !(sol.retcode == :Success)
         @addlogprob! -Inf
         return
     end
     @addlogprob! solve(sol, x0, (0, T); noise = ϵ, observables = z).logpdf
 end
-turing_model = rbc_second(fake_z_second, model_rbc_second, p_f, allocate_cache(model_rbc_second))
+c = SolverCache(model_rbc, Val(2), p_d)
+turing_model = rbc_joint(fake_z, model_rbc, p_f, c)
 n_samples = 20
 n_adapts = 5
 δ = 0.65
 max_depth = 2
 chain = sample(turing_model, NUTS(n_adapts, δ; max_depth), n_samples; progress = true)
-
-m = @include_example_module(Examples.rbc)
