87a5a08fbd80037ed94ef64fa7a1a0c279e57530
Peifan Wu <wupeifan@gmail.com>
2021-10-28 01:11:17 -0700

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,18 +5,18 @@ using Test, LinearAlgebra
 # This has no effect with MKL
 DifferentiableStateSpaceModels.set_blas_threads()
 
-# println(
-#    "Running Testsuite with Threads.nthreads() = $(Threads.nthreads()) BLAS.vendor = $(BLAS.vendor()), and BLAS.num_threads = $(BLAS.get_num_threads()) \n",
-# )
+println(
+   "Running Testsuite with Threads.nthreads() = $(Threads.nthreads()) BLAS.vendor = $(BLAS.vendor()), and BLAS.num_threads = $(BLAS.get_num_threads()) \n",
+)
 
 # Delete the .function_cache
 # e.g. ENV["DSSM_TEST_DELETE_CACHE"] = "false" environment variable to turn off, can be global
-# get(ENV, "DSSM_TEST_DELETE_CACHE", "true") == "true" &&
-#    rm(default_model_cache_location(), force = true, recursive = true)
+get(ENV, "DSSM_TEST_DELETE_CACHE", "true") == "true" &&
+   rm(default_model_cache_location(), force = true, recursive = true)
 
-# include("make_perturbation_model.jl")
-# include("first_order_perturbation.jl")
-# include("second_order_perturbation.jl")
+include("make_perturbation_model.jl")
+include("first_order_perturbation.jl")
+include("second_order_perturbation.jl")
 include("first_order_sequence.jl")
 include("second_order_sequence.jl")
 # include("rbc_estimation.jl")
