805b255816e5753f5c6ee82f648f1b1df9b7685b
Peifan Wu <wupeifan@gmail.com>
2021-10-29 11:19:47 -0700

    Update rbc_estimation.jl

--- test/rbc_estimation.jl
+++ test/rbc_estimation.jl
@@ -1,12 +1,11 @@
 using DifferentiableStateSpaceModels, LinearAlgebra, Optim, Test, Turing, Zygote
-using ChainRulesCore
 using DifferentiableStateSpaceModels.Examples
 using Turing: @addlogprob!
 
 Turing.setadbackend(:zygote)
 
 # Create models from modules and then solve
-#p_d = [α, β]
+# p_d = [α, β]
 model_rbc = @include_example_module(Examples.rbc_observables)
 
 p_f = (ρ=0.2, δ=0.02, σ=0.01, Ω_1=0.01)
@@ -43,26 +42,7 @@ n_adapts = 5
 chain = sample(turing_model, NUTS(n_adapts, δ), n_samples; progress = true)
 θ_MAP = optimize(turing_model, MAP())
 
-# NOTE: a function wrap doesn't really help. It's a more general Zygote issue.
-# function generate_perturbation_wrap(m, p_d_input, p_f, order::Val{1}; cache)
-#     p_d = (α = p_d_input[1], β = p_d_input[2])
-#     sol = generate_perturbation(m, p_d, p_f, order; cache)
-#     return sol
-# end
-
-# function ChainRulesCore.rrule(::typeof(generate_perturbation_wrap), m, p_d_input, p_f, order::Val{1}; cache)
-#     println("using wrap 1")
-#     p_d = (α = p_d_input[1], β = p_d_input[2])
-#     sol, pb = ChainRulesCore.rrule(generate_perturbation, m, p_d, p_f, order; cache)
-#     function wrap_pb(Δsol)
-#         println("using wrap 2")
-#         res = pb(Δsol)
-#         return res[1], res[2], reduce(vcat, res[3]), res[4], res[5]
-#     end
-#     return sol, wrap_pb
-# end
-
-## Turing model, Joint likelihood
+# Turing model, Joint likelihood
 @model function rbc_joint(z, m, p_f, cache, x0 = zeros(m.n_x))
     α ~ Uniform(0.2, 0.8)
     β ~ Uniform(0.5, 0.99)
@@ -103,7 +83,7 @@ g = gradient(density, [p;vcat(ϵ...)])
 =#
 # FiniteDiff.finite_difference_gradient(density, [p;vcat(ϵ...)])
 
-## Turing model, Joint likelihood, Second-order
+# Turing model, Joint likelihood, Second-order
 @model function rbc_second(z, m, p_f, cache, x0 = zeros(m.n_x))
     α ~ Uniform(0.2, 0.8)
     β ~ Uniform(0.5, 0.99)
