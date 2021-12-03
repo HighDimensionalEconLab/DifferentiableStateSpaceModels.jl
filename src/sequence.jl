9cf0499e11677b73be74ee82e9c487b504d0eb33
Cameron Pfiffer <cpfiffer@gmail.com>
2021-12-03 12:47:04 -0800

    Added second-order back

--- src/sequence.jl
+++ src/sequence.jl
@@ -3,31 +3,25 @@
 
 # First order
 dssm_evolution(u, sol::FirstOrderPerturbationSolution, t) = sol.A * u # f
-dssm_volatility(u, sol::FirstOrderPerturbationSolution, t) = sol.B    # g
+dssm_volatility(u, sol::FirstOrderPerturbationSolution, t) = sol.B'    # g
 dssm_observation(u, sol::FirstOrderPerturbationSolution, t) = sol.C * u # h
 
-params = (a = 1, b=2, σ=6)
-f(u, p, t)
-g(u, p, t)
-h(u, p, t)
-
-f([1,1], params, 1)
 # # The 2nd order are trickier because of state-space pruning TODO: Move these over to DifferenceEquations.jl
-# function dssm_evolution(x, sol::SecondOrderPerturbationSolution, t)
-#     # assume x is stacked as [x_f, x]
-#     # Try a view?
-#     x_f_new = sol.A_1 * x[1:sol.n_x]
-#     x_new = sol.A_0 + sol.A_1 * x[(sol.n_x+1):end] + quad(sol.A_2, x[1:sol.n_x])
-#     return [x_f_new; x_new]
-# end
+function dssm_evolution(x, sol::SecondOrderPerturbationSolution, t)
+    # assume x is stacked as [x_f, x]
+    # Try a view?
+    x_f_new = sol.A_1 * x[1:sol.n_x]
+    x_new = sol.A_0 + sol.A_1 * x[(sol.n_x+1):end] + quad(sol.A_2, x[1:sol.n_x])
+    return [x_f_new; x_new]
+end
 
-# dssm_volatility(x, sol::SecondOrderPerturbationSolution, t) = [sol.B; sol.B]
+dssm_volatility(x, sol::SecondOrderPerturbationSolution, t) = [sol.B; sol.B]
 
-# function dssm_observation(x, sol::SecondOrderPerturbationSolution, t)
-#     # assume x is stacked as [x_f, x]
-#     # Try a view?
-#     return sol.C_0 + sol.C_1 * x[(sol.n_x+1):end] + quad(sol.C_2, x[1:sol.n_x])
-# end
+function dssm_observation(x, sol::SecondOrderPerturbationSolution, t)
+    # assume x is stacked as [x_f, x]
+    # Try a view?
+    return sol.C_0 + sol.C_1 * x[(sol.n_x+1):end] + quad(sol.C_2, x[1:sol.n_x])
+end
 
 
 
