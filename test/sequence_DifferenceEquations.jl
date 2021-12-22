a0c774902aac845444af496aba11868bc6be511e
Peifan Wu <wupeifan@gmail.com>
2021-12-22 10:22:17 -0800

    Update sequence_DifferenceEquations.jl

--- test/sequence_DifferenceEquations.jl
+++ test/sequence_DifferenceEquations.jl
@@ -93,19 +93,6 @@ end
                     [26.06697625612332], [33.10959536324157], [31.484308705831474],
                     [19.172319198105615], [11.464791870737214], [-0.9477420442978448]]
 
-    sol = generate_perturbation(m, p_d, p_f, Val(1))
-    problem = StateSpaceProblem(
-        DifferentiableStateSpaceModels.dssm_evolution,
-        DifferentiableStateSpaceModels.dssm_volatility,
-        DifferentiableStateSpaceModels.dssm_observation,
-        x0,
-        tspan,
-        sol,
-        noise = DefinedNoise(ϵ),
-        obs_noise = sol.D,
-        observables = z
-    )
-
     # inference
     # sol = generate_perturbation(m, p_d, p_f)
     # @inferred solve(
@@ -181,4 +168,52 @@ end
     @test simul.z ≈
           DifferentiableStateSpaceModels.solve(sol, x0, (0, T), DifferentiableStateSpaceModels.QTI(); noise = eps_value).z
     @inferred DifferenceEquations.solve(problem, ConditionalGaussian())
+end
+
+function likelihood_test_joint_second(p_d_input, p_f, ϵ, x0, m, tspan, z)
+    p_d = (α = p_d_input[1], β = p_d_input[2])
+    sol = generate_perturbation(m, p_d, p_f, Val(2))
+    problem = StateSpaceProblem(
+        DifferentiableStateSpaceModels.dssm_evolution,
+        DifferentiableStateSpaceModels.dssm_volatility,
+        DifferentiableStateSpaceModels.dssm_observation,
+        x0,
+        tspan,
+        sol,
+        noise = DefinedNoise(ϵ),
+        obs_noise = sol.D,
+        observables = z
+    )
+    return DifferenceEquations.solve(problem, ConditionalGaussian(); vectype = Zygote.Buffer).loglikelihood
+end
+
+@testset "Gradients, generate_perturbation + likelihood, 2nd order" begin
+    m = @include_example_module(Examples.rbc_observables)
+    p_f = (ρ = 0.2, δ = 0.02, σ = 0.01, Ω_1 = 0.1)
+    p_d = (α = 0.5, β = 0.95)
+    p_d_input = [0.5, 0.95]
+    ϵ = [[0.22], [0.01], [0.14], [0.03], [0.15], [0.21], [0.22], [0.05], [0.18]]
+    z = [[-0.6949847708598687, -0.8456988740809867],
+         [-0.7804117657996692, 0.07781473603479207],
+         [-1.1363021614363802, -2.41253450179418],
+         [-0.2140813001516194, -0.10914617826240575],
+         [-1.0365874981404577, 0.9869373465251516],
+         [-0.7321498641416826, 0.012293325072265942],
+         [-0.054809260599132194, -1.8233591236618099],
+         [0.5407452466493482, -0.9773559802938866],
+         [1.3968232347532277, -2.139194998843768]]
+    x0 = zeros(2 * m.n_x)
+    tspan = (0, length(z))
+
+    res = gradient((p_d_input, ϵ) -> likelihood_test_joint_second(p_d_input, p_f, ϵ, x0, m,
+                                                                  tspan, z), p_d_input, ϵ)
+    @test res[1] ≈ [305.5874661276336, 559.166700806099]
+    @test res[2] ≈ [[40.5141940179588], [39.32706019833505], [25.02785099195666],
+                    [26.010688843169483], [33.01985483763039], [31.381238099783715],
+                    [19.106378855992403], [11.441562042277948], [-0.9454627257067805]]
+
+
+    # inferred
+    # @inferred likelihood_test_joint_second(p_d_input, p_f, ϵ, x0, m, tspan, z)
+    # @inferred likelihood_test_joint_second_sol(p_d_input, p_f, ϵ, x0, m, tspan, z)
 end
