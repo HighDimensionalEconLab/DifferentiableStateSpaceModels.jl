7f03464b1072b3cc4783abcb8f8405d1f7965071
Peifan Wu <wupeifan@gmail.com>
2021-12-22 10:08:56 -0800

    downstream test

---
+++ test/sequence_DifferenceEquations.jl
@@ -0,0 +1,184 @@
+# Test the downstream `solve` in DifferenceEquations.jl
+
+using DifferentiableStateSpaceModels, DifferenceEquations, LinearAlgebra, Test, Zygote
+using CSV, DataFrames, Turing, DistributionsAD
+using DifferentiableStateSpaceModels.Examples
+using FiniteDiff: finite_difference_gradient
+
+@testset "Sequence Simulation, 1st order" begin
+    m = @include_example_module(Examples.rbc_observables)
+    p_f = (ρ = 0.2, δ = 0.02, σ = 0.01, Ω_1 = 0.1)
+    p_d = (α = 0.5, β = 0.95)
+
+    c = SolverCache(m, Val(1), p_d)
+    sol = generate_perturbation(m, p_d, p_f; cache = c)
+
+    T = 9
+    eps_value = [[0.22], [0.01], [0.14], [0.03], [0.15], [0.21], [0.22], [0.05], [0.18]]
+    obs_noise = [zeros(2) for _ in 1:T] # there is no observation noises
+    x0 = zeros(m.n_x)
+
+    problem = StateSpaceProblem(
+        DifferentiableStateSpaceModels.dssm_evolution,
+        DifferentiableStateSpaceModels.dssm_volatility,
+        DifferentiableStateSpaceModels.dssm_observation,
+        x0,
+        (0, T),
+        sol,
+        noise = DefinedNoise(eps_value),
+        obs_noise = DefinedNoise(obs_noise)
+    )
+    
+    # Solve the model, this generates
+    # simulated data.
+    simul = DifferenceEquations.solve(problem, ConditionalGaussian())
+    # A hard code test copied from sequence.jl
+    @test simul.z[2:end] ≈
+          [[-0.0014843113235688628, 0.0], [-0.001672969226342977, -0.013660616212663025],
+           [-0.0025907902434120613, -0.016424018091334355],
+           [-0.002808352075911724, -0.02507880927301923],
+           [-0.003749820417827369, -0.027731843802628737],
+           [-0.00514124775142971, -0.036595970922849726],
+           [-0.006607765317710925, -0.05006822400565087],
+           [-0.006885970365182856, -0.06457803532896965],
+           [-0.00789049635407183, -0.06822941930528102]]
+    # A comparison to the old `solve` from sequence.jl
+    @test simul.z ≈
+          DifferentiableStateSpaceModels.solve(sol, x0, (0, T), DifferentiableStateSpaceModels.LTI(); noise = eps_value).z
+
+    # inference
+    @inferred DifferenceEquations.solve(problem, ConditionalGaussian())
+    @inferred generate_perturbation(m, p_d, p_f; cache = c)
+end
+
+function likelihood_test_joint_first(p_d_input, p_f, ϵ, x0, m, tspan, z)
+    p_d = (α = p_d_input[1], β = p_d_input[2])
+    sol = generate_perturbation(m, p_d, p_f, Val(1))
+    problem = StateSpaceProblem(
+        DifferentiableStateSpaceModels.dssm_evolution,
+        DifferentiableStateSpaceModels.dssm_volatility,
+        DifferentiableStateSpaceModels.dssm_observation,
+        x0,
+        tspan,
+        sol,
+        noise = DefinedNoise(ϵ),
+        obs_noise = sol.D,
+        observables = z
+    )
+    return DifferenceEquations.solve(problem, ConditionalGaussian(); vectype = Zygote.Buffer).loglikelihood
+end
+
+@testset "Gradients, generate_perturbation + likelihood, 1st order" begin
+    m = @include_example_module(Examples.rbc_observables)
+    p_f = (ρ = 0.2, δ = 0.02, σ = 0.01, Ω_1 = 0.1)
+    p_d = (α = 0.5, β = 0.95)
+    p_d_input = [0.5, 0.95]
+    ϵ = [[0.22], [0.01], [0.14], [0.03], [0.15], [0.21], [0.22], [0.05], [0.18]]
+    z = [[-0.6949847708598687, -0.8456988740809867],
+         [-0.7804117657996692, 0.07781473603479207],
+         [-1.1363021614363802, -2.41253450179418],
+         [-0.2140813001516194, -0.10914617826240575],
+         [-1.0365874981404577, 0.9869373465251516],
+         [-0.7321498641416826, 0.012293325072265942],
+         [-0.054809260599132194, -1.8233591236618099],
+         [0.5407452466493482, -0.9773559802938866],
+         [1.3968232347532277, -2.139194998843768]]
+    x0 = zeros(m.n_x)
+    tspan = (0, length(z))
+
+    res = gradient((p_d_input, ϵ) -> likelihood_test_joint_first(p_d_input, p_f, ϵ, x0, m,
+                                                                 tspan, z), p_d_input, ϵ)
+    @test res[1] ≈ [303.7133186356109, 553.6149537473261]
+    @test res[2] ≈ [[40.62454806083384], [39.38899479341156], [25.095297618483304],
+                    [26.06697625612332], [33.10959536324157], [31.484308705831474],
+                    [19.172319198105615], [11.464791870737214], [-0.9477420442978448]]
+
+    sol = generate_perturbation(m, p_d, p_f, Val(1))
+    problem = StateSpaceProblem(
+        DifferentiableStateSpaceModels.dssm_evolution,
+        DifferentiableStateSpaceModels.dssm_volatility,
+        DifferentiableStateSpaceModels.dssm_observation,
+        x0,
+        tspan,
+        sol,
+        noise = DefinedNoise(ϵ),
+        obs_noise = sol.D,
+        observables = z
+    )
+
+    # inference
+    # sol = generate_perturbation(m, p_d, p_f)
+    # @inferred solve(
+    #     DifferentiableStateSpaceModels.dssm_evolution,
+    #     DifferentiableStateSpaceModels.dssm_volatility,
+    #     x0,
+    #     tspan,
+    #     sol;
+    #     observables = z,
+    #     h = DifferentiableStateSpaceModels.dssm_observation,
+    #     sol.D,
+    #     noise = ϵ
+    # ).logpdf
+    # @inferred likelihood_test_joint_first(p_d_input, p_f, ϵ, x0, m, tspan, z)
+end
+
+@testset "Sequence Simulation, 2nd order" begin
+    m = @include_example_module(Examples.rbc_observables)
+    p_f = (ρ = 0.2, δ = 0.02, σ = 0.01, Ω_1 = 0.1)
+    p_d = (α = 0.5, β = 0.95)
+
+    c = SolverCache(m, Val(1), p_d)
+    sol = generate_perturbation(m, p_d, p_f; cache = c)
+
+    T = 9
+    eps_value = [[0.22], [0.01], [0.14], [0.03], [0.15], [0.21], [0.22], [0.05], [0.18]]
+    obs_noise = [zeros(2) for _ in 1:T] # there is no observation noises
+    x0 = zeros(m.n_x)
+
+    problem = StateSpaceProblem(
+        DifferentiableStateSpaceModels.dssm_evolution,
+        DifferentiableStateSpaceModels.dssm_volatility,
+        DifferentiableStateSpaceModels.dssm_observation,
+        x0,
+        (0, T),
+        sol,
+        noise = DefinedNoise(eps_value),
+        obs_noise = DefinedNoise(obs_noise)
+    )
+    
+    m = @include_example_module(Examples.rbc_observables)
+    p_f = (ρ = 0.2, δ = 0.02, σ = 0.01, Ω_1 = 0.1)
+    p_d = (α = 0.5, β = 0.95)
+
+    c = SolverCache(m, Val(2), p_d)
+    sol = generate_perturbation(m, p_d, p_f, Val(2); cache = c)
+
+    T = 9
+    eps_value = [[0.22], [0.01], [0.14], [0.03], [0.15], [0.21], [0.22], [0.05], [0.18]]
+    obs_noise = [zeros(2) for _ in 1:T] # there is no observation noises
+    x0 = zeros(m.n_x)
+    problem = StateSpaceProblem(
+        DifferentiableStateSpaceModels.dssm_evolution,
+        DifferentiableStateSpaceModels.dssm_volatility,
+        DifferentiableStateSpaceModels.dssm_observation,
+        [x0; x0],
+        (0, T),
+        sol,
+        noise = DefinedNoise(eps_value),
+        obs_noise = DefinedNoise(obs_noise)
+    )
+    simul = DifferenceEquations.solve(problem, ConditionalGaussian())
+    @test simul.z[2:end] ≈ [[-0.0014120420256672264, -7.824904812715083e-5],
+                            [-0.001607843339241866, -0.013798593509356017],
+                            [-0.0025317633821568975, -0.016632915260522855],
+                            [-0.002755836083102567, -0.02534820495624617],
+                            [-0.0037026560860619877, -0.028065833613511802],
+                            [-0.005097998299327853, -0.036982697654476426],
+                            [-0.006567668012302603, -0.05049239849879983],
+                            [-0.006851208817373075, -0.06503101829364497],
+                            [-0.007859486666066144, -0.06873403795558215]]
+    # Compare with old sequence.jl stuff
+    @test simul.z ≈
+          DifferentiableStateSpaceModels.solve(sol, x0, (0, T), DifferentiableStateSpaceModels.QTI(); noise = eps_value).z
+    @inferred DifferenceEquations.solve(problem, ConditionalGaussian())
+end
