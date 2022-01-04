24600da9941e6a12ff992a19bb93c59ee9e6e714
Peifan Wu <wupeifan@gmail.com>
2022-01-04 15:57:46 -0800

    primal checked

--- src/sequence.jl
+++ src/sequence.jl
@@ -118,17 +118,55 @@ function _solve(alg::GeneralLikelihood, noise, observables, f, g, h, D, u0, tspa
     return StateSpaceSolution(nothing, nothing, nothing, nothing, loglik)
 end
 
-function ChainRulesCore.rrule(::typeof(_solve), alg::GeneralLikelihood, noise, observables, f, g, h, D, u0, tspan, p,
+function ChainRulesCore.rrule(::typeof(_solve), alg::GeneralLikelihood, noise, observables, f, g, h, D, u0, tspan,
+                                p::FirstOrderPerturbationSolution,
                                 save_everystep, save_posteriors, calculate_logpdf, simulate_observation_noise)
     println("Action!")
-    sol = 0
+    # Primal computation
+    T = tspan[2]
+
+    z0 = h(u0, p, 0)
+    g0 = g(u0, p, 0)
+    u = Vector{typeof(u0)}(undef, T + 1)
+    z = Vector{typeof(z0)}(undef, T + 1)
+    g_primal = Vector{typeof(g0)}(undef, T + 1)
+    f_pb = Vector{Function}(undef, T + 1)
+    g_pb = Vector{Function}(undef, T + 1)
+    h_pb = Vector{Function}(undef, T + 1)
+    u[1] = u0
+    z[1] = z0
+    loglik = 0.0  # remains 0 if no observables
+    for t in 2:T+1
+        # Fetch the pullback functions when iterating the evolution forward
+        tmpf, f_pb[t] = Zygote.pullback(f, u[t - 1], p, t - 1)
+        g_primal[t], g_pb[t] = Zygote.pullback(g, u[t - 1], p, t - 1)
+        u[t] = tmpf + g_primal[t] * noise[t - 1]
+        z[t], h_pb[t] = Zygote.pullback(h, u[t], p, t)
+        loglik += logpdf(D, observables[t - 1] - z[t])  # z_0 doesn't enter likelihood        
+    end
+    sol = StateSpaceSolution(nothing, nothing, nothing, nothing, loglik)
+    
     function solve_pb(Δsol)
-        Δp = deepcopy(p)
-        return NoTangent(), NoTangent(), NoTangent(), NoTangent(), NoTangent(), NoTangent(), NoTangent(), NoTangent(), NoTangent(), NoTangent(), Δp,
-        NoTangent(), NoTangent(), NoTangent(), NoTangent()
+        Δlogpdf = Δsol.logpdf
+        ΔA = similar(p.A)
+        ΔB = similar(p.B)
+        ΔC = similar(p.C)
+        fill!(ΔA, 0)
+        fill!(ΔB, 0)
+        fill!(ΔC, 0)
+
+        for t in (T+1):-1:2
+            
+        end 
+
+        return NoTangent(), NoTangent(), NoTangent(), NoTangent(), NoTangent(), NoTangent(), NoTangent(), NoTangent(), NoTangent(), NoTangent(),
+               Tangent{typeof(p)}(; A = ΔA, B = ΔB, C = ΔC)
+               NoTangent(), NoTangent(), NoTangent(), NoTangent()
     end
     return sol, solve_pb
 end
+
+Tangent{typeof(sol)}(; A = ones(3, 3))
 
 ############################################
 # LTI Specializations
