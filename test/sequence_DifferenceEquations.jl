c9c891f1eb266f1e2ddecd0a46528785ed34efd6
Peifan Wu <wupeifan@gmail.com>
2022-01-05 15:27:53 -0800

    add tests

--- test/sequence_DifferenceEquations.jl
+++ test/sequence_DifferenceEquations.jl
@@ -1,7 +1,7 @@
 # Test the downstream `solve` in DifferenceEquations.jl
 
 using DifferentiableStateSpaceModels, DifferenceEquations, LinearAlgebra, Test, Zygote
-using Turing, DistributionsAD
+using Distributions, DistributionsAD
 using DifferentiableStateSpaceModels.Examples
 
 @testset "Sequence Simulation, 1st order" begin
@@ -16,6 +16,7 @@ using DifferentiableStateSpaceModels.Examples
     eps_value = [[0.22], [0.01], [0.14], [0.03], [0.15], [0.21], [0.22], [0.05], [0.18]]
     x0 = zeros(m.n_x)
 
+    # General simulation
     problem = StateSpaceProblem(
         DifferentiableStateSpaceModels.dssm_evolution,
         DifferentiableStateSpaceModels.dssm_volatility,
@@ -23,15 +24,12 @@ using DifferentiableStateSpaceModels.Examples
         x0,
         (0, T),
         sol,
-        noise = DefinedNoise(eps_value)
-    )
-    
-    # Solve the model, this generates
-    # simulated data.
+        noise = eps_value
+    )    
     simul = DifferenceEquations.solve(problem, NoiseConditionalFilter())
-    # A hard code test copied from sequence.jl
     @test simul.z[2:end] ≈
-          [[-0.0014843113235688628, 0.0], [-0.001672969226342977, -0.013660616212663025],
+          [[-0.0014843113235688628, 0.0],
+           [-0.001672969226342977, -0.013660616212663025],
            [-0.0025907902434120613, -0.016424018091334355],
            [-0.002808352075911724, -0.02507880927301923],
            [-0.003749820417827369, -0.027731843802628737],
@@ -39,16 +37,35 @@ using DifferentiableStateSpaceModels.Examples
            [-0.006607765317710925, -0.05006822400565087],
            [-0.006885970365182856, -0.06457803532896965],
            [-0.00789049635407183, -0.06822941930528102]]
-    # A comparison to the old `solve` from sequence.jl
-    @test simul.z ≈
-          DifferentiableStateSpaceModels.solve(sol, x0, (0, T), DifferentiableStateSpaceModels.LTI(); noise = eps_value).z
+        
+    # Linear specific problem
+    linear_problem = LinearStateSpaceProblem(
+        sol.A,
+        sol.B,
+        sol.C,
+        x0,
+        (0, T),
+        noise = eps_value
+    )
+    simul_linear = DifferenceEquations.solve(linear_problem, NoiseConditionalFilter())
+    @test simul_linear.z[2:end] ≈
+            [[-0.0014843113235688628, 0.0],
+             [-0.001672969226342977, -0.013660616212663025],
+             [-0.0025907902434120613, -0.016424018091334355],
+             [-0.002808352075911724, -0.02507880927301923],
+             [-0.003749820417827369, -0.027731843802628737],
+             [-0.00514124775142971, -0.036595970922849726],
+             [-0.006607765317710925, -0.05006822400565087],
+             [-0.006885970365182856, -0.06457803532896965],
+             [-0.00789049635407183, -0.06822941930528102]]
 
     # inference
     @inferred DifferenceEquations.solve(problem, NoiseConditionalFilter())
+    @inferred DifferenceEquations.solve(linear_problem, NoiseConditionalFilter())
     @inferred generate_perturbation(m, p_d, p_f; cache = c)
 end
 
-function likelihood_test_joint_first(p_d_input, p_f, ϵ, x0, m, tspan, z)
+function likelihood_test_joint_first_general(p_d_input, p_f, ϵ, x0, m, tspan, z)
     p_d = (α = p_d_input[1], β = p_d_input[2])
     sol = generate_perturbation(m, p_d, p_f, Val(1))
     problem = StateSpaceProblem(
@@ -58,11 +75,27 @@ function likelihood_test_joint_first(p_d_input, p_f, ϵ, x0, m, tspan, z)
         x0,
         tspan,
         sol,
-        noise = DefinedNoise(ϵ),
+        noise = ϵ,
         obs_noise = sol.D,
         observables = z
     )
-    return DifferenceEquations.solve(problem, NoiseConditionalFilter(); vectype = Zygote.Buffer).loglikelihood
+    return DifferenceEquations.solve(problem, NoiseConditionalFilter()).loglikelihood
+end
+
+function likelihood_test_joint_first_linear(p_d_input, p_f, ϵ, x0, m, tspan, z)
+    p_d = (α = p_d_input[1], β = p_d_input[2])
+    sol = generate_perturbation(m, p_d, p_f, Val(1))
+    problem = LinearStateSpaceProblem(
+        sol.A,
+        sol.B,
+        sol.C,
+        x0,
+        tspan,
+        noise = ϵ,
+        obs_noise = sol.D,
+        observables = z
+    )
+    return DifferenceEquations.solve(problem, NoiseConditionalFilter()).loglikelihood
 end
 
 @testset "Gradients, generate_perturbation + likelihood, 1st order" begin
@@ -83,27 +116,18 @@ end
     x0 = zeros(m.n_x)
     tspan = (0, length(z))
 
-    res = gradient((p_d_input, ϵ) -> likelihood_test_joint_first(p_d_input, p_f, ϵ, x0, m,
-                                                                 tspan, z), p_d_input, ϵ)
+    res = gradient((p_d_input, ϵ) -> likelihood_test_joint_first_general(p_d_input, p_f, ϵ, x0, m, tspan, z), p_d_input, ϵ)
     @test res[1] ≈ [303.7133186356109, 553.6149537473261]
     @test res[2] ≈ [[40.62454806083384], [39.38899479341156], [25.095297618483304],
                     [26.06697625612332], [33.10959536324157], [31.484308705831474],
                     [19.172319198105615], [11.464791870737214], [-0.9477420442978448]]
 
-    # inference
-    # sol = generate_perturbation(m, p_d, p_f)
-    # @inferred solve(
-    #     DifferentiableStateSpaceModels.dssm_evolution,
-    #     DifferentiableStateSpaceModels.dssm_volatility,
-    #     x0,
-    #     tspan,
-    #     sol;
-    #     observables = z,
-    #     h = DifferentiableStateSpaceModels.dssm_observation,
-    #     sol.D,
-    #     noise = ϵ
-    # ).logpdf
-    # @inferred likelihood_test_joint_first(p_d_input, p_f, ϵ, x0, m, tspan, z)
+    res = gradient((p_d_input, ϵ) -> likelihood_test_joint_first_linear(p_d_input, p_f, ϵ, x0, m, tspan, z), p_d_input, ϵ)
+    @test res[1] ≈ [303.7133186356109, 553.6149537473261]
+    @test res[2] ≈ [[40.62454806083384], [39.38899479341156], [25.095297618483304],
+                    [26.06697625612332], [33.10959536324157], [31.484308705831474],
+                    [19.172319198105615], [11.464791870737214], [-0.9477420442978448]]
+
 end
 
 @testset "Sequence Simulation, 2nd order" begin
