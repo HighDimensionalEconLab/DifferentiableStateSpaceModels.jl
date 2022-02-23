49fef53f6cd9a60071dca83f9257c9d74033ac5d
Jesse Perla <jesseperla@gmail.com>
2022-02-23 13:00:01 -0800

    Finalized unit test cleanup

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,13 @@ using Test, LinearAlgebra
 # Delete the .function_cache
 rm(default_model_cache_location(); force = true, recursive = true)
 
-@time include("make_perturbation_model.jl")
-@time include("first_order_perturbation.jl")
-@time include("second_order_perturbation.jl")
-@time include("first_order_sequence.jl")
-get(ENV, "CI", "false") == "false" && @time include("second_order_sequence.jl")
-get(ENV, "CI", "false") == "false" && @time include("sgu.jl")
-get(ENV, "CI", "false") == "false" && @time include("FVGQ20.jl")
-@time include("symbolic_utils.jl")
-@time include("utils.jl")
+include("make_perturbation_model.jl")
+include("first_order_perturbation.jl")
+include("second_order_perturbation.jl")
+include("first_order_sequence.jl")
+include("second_order_sequence.jl")
+include("symbolic_utils.jl")
+include("utils.jl")
+
+# only add this in temporarily during devleopment
+# include("slow_tests.jl")
