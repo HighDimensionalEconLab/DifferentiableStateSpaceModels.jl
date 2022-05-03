75f5cc58bf29c4408f9c4c5b369a77c7c345edb9
Peifan Wu <wupeifan@gmail.com>
2022-05-03 14:09:29 -0700

    update model

--- src/Examples/SW07.jl
+++ src/Examples/SW07.jl
@@ -35,24 +35,15 @@ function SW07()
     # γ_bar = 0.3982
     # gy_ss = 0.18
 
-    # Define symbolic ones
-    @parameters ε_w ρ_ga ε_p l_bar Π_bar B μ_w μ_p α ψ φ δ σ_c λ ϕ_p ι_w ξ_w ι_p ξ_p σ_l ϕ_w r_π r_Δy r_y ρ ρ_a ρ_b ρ_g ρ_i ρ_r ρ_p ρ_w γ_bar gy_ss # 34 parameters
+    ∞ = Inf
+    # Parameters
+    @variables ε_w, ρ_ga, ε_p, l_bar, Π_bar, B, μ_w, μ_p, α, ψ, φ, δ, σ_c, λ, ϕ_p, ι_w, ξ_w, ι_p, ξ_p, σ_l, ϕ_w, r_π, r_Δy, r_y, ρ, ρ_a, ρ_b, ρ_g, ρ_i, ρ_r, ρ_p, ρ_w, γ_bar, gy_ss # 34 parameters
     # states
-    @variables y_f_m y_m k_f k c_f_m c_m i_f_m i_m π_m w_m r_m ε_a b ε_g ε_i ε_r ε_pm η_p_aux ε_wm η_w_aux # 20 variables
+    @variables t::Integer, y_f_m(..), y_m(..), k_f(..), k(..), c_f_m(..), c_m(..), i_f_m(..), i_m(..), π_m(..), w_m(..), r_m(..), ε_a(..), b(..), ε_g(..), ε_i(..), ε_r(..), ε_pm(..), η_p_aux(..), ε_wm(..), η_w_aux(..) # 20 variables
     # controls
-    @variables k_s_f k_s r_f r r_k_f r_k z_f z w_f w l_f l q_f q y_f y i_f i c_f c π μ_pm # 22 variables
-    # Markovize
-    @make_markov y_f_m y_m k_f k c_f_m c_m i_f_m i_m π_m w_m r_m ε_a b ε_g ε_i ε_r ε_pm η_p_aux ε_wm η_w_aux k_s_f k_s r_f r r_k_f r_k z_f z w_f w l_f l q_f q y_f y i_f i c_f c π μ_pm # 42 variables
+    @variables k_s_f(..), k_s(..), r_f(..), r(..), r_k_f(..), r_k(..), z_f(..), z(..), w_f(..), w(..), l_f(..), l(..), q_f(..), q(..), y_f(..), y(..), i_f(..), i(..), c_f(..), c(..), π(..), μ_pm(..) # 22 variables
 
-    x_sym = [y_f_m, y_m, k_f, k, c_f_m, c_m, i_f_m, i_m, π_m, w_m, r_m, ε_a, b, ε_g, ε_i,
-             ε_r, ε_pm, η_p_aux, ε_wm, η_w_aux]
-    y_sym = [k_s_f, k_s, r_f, r, r_k_f, r_k, z_f, z, w_f, w, l_f, l, q_f, q, y_f, y, i_f, i,
-             c_f, c, π, μ_pm]
-    p = [ε_w, ρ_ga, ε_p, l_bar, Π_bar, B, μ_w, μ_p, α, ψ, φ, δ, σ_c, λ, ϕ_p, ι_w, ξ_w, ι_p,
-         ξ_p, σ_l, ϕ_w, r_π, r_Δy, r_y, ρ, ρ_a, ρ_b, ρ_g, ρ_i, ρ_r, ρ_p, ρ_w, γ_bar, gy_ss]
-    p_f = []
-
-    # Pre-compute variables
+    # Pre-compute parameters
     π_bar = 1 + Π_bar / 100
     γ = 1 + γ_bar / 100
     β = 1 / (1 + B / 100)
@@ -69,69 +60,59 @@ function SW07()
     z_y = R_k_star * k_y
     w_hlc = 1 / ϕ_w * (1 - α) / α * R_k_star * k_y / c_y
 
+    # States and controls
+    x_sym = [y_f_m, y_m, k_f, k, c_f_m, c_m, i_f_m, i_m, π_m, w_m, r_m, ε_a, b, ε_g, ε_i, ε_r, ε_pm, η_p_aux, ε_wm, η_w_aux]
+    y_sym = [k_s_f, k_s, r_f, r, r_k_f, r_k, z_f, z, w_f, w, l_f, l, q_f, q, y_f, y, i_f, i, c_f, c, π, μ_pm]
+    # Merge p and p_f
+    p = [ε_w, ρ_ga, ε_p, l_bar, Π_bar, B, μ_w, μ_p, α, ψ, φ, δ, σ_c, λ, ϕ_p, ι_w, ξ_w, ι_p, ξ_p, σ_l, ϕ_w, r_π, r_Δy, r_y, ρ, ρ_a, ρ_b, ρ_g, ρ_i, ρ_r, ρ_p, ρ_w, γ_bar, gy_ss]
+
     # Defining H
-    H = [α * r_k_f + (1 - α) * w_f - ε_a, (1 - ψ) / ψ * r_k_f - z_f,
-         w_f + l_f - k_s_f - r_k_f, k_f + z_f - k_s_f,
-         1 / (1 + β * γ) * (i_f_m + β * γ * i_f_p + 1 / (γ^2 * φ) * q_f) + ε_i - i_f,
-         -r_f +
-         b / ((1 - λ / γ) / (σ_c * (1 + λ / γ))) +
-         R_k_star / (R_k_star + 1 - δ) * r_k_f_p +
-         (1 - δ) / (R_k_star + 1 - δ) * q_f_p - q_f,
-         λ / γ / (1 + λ / γ) * c_f_m +
-         1 / (1 + λ / γ) * c_f_p +
-         (σ_c - 1) * w_hlc / (σ_c * (1 + λ / γ)) * (l_f - l_f_p) -
-         (1 - λ / γ) / (σ_c * (1 + λ / γ)) * r_f + b - c_f,
-         c_y * c_f + i_y * i_f + ε_g + z_y * z_f - y_f,
-         ϕ_p * (α * k_s_f + (1 - α) * l_f + ε_a) - y_f,
-         σ_l * l_f + 1 / (1 - λ / γ) * c_f - λ / γ / (1 - λ / γ) * c_f_m - w_f,
-         (1 - k_1) * k_f + k_1 * i_f + k_1 * γ^2 * φ * ε_i - k_f_p,
+    H = [α * r_k_f(t) + (1 - α) * w_f(t) - ε_a(t), (1 - ψ) / ψ * r_k_f(t) - z_f(t),
+         w_f(t) + l_f(t) - k_s_f(t) - r_k_f(t), k_f(t) + z_f(t) - k_s_f(t),
+         1 / (1 + β * γ) * (i_f_m(t) + β * γ * i_f(t+1) + 1 / (γ^2 * φ) * q_f(t)) + ε_i(t) - i_f(t),
+         -r_f(t) +
+         b(t) / ((1 - λ / γ) / (σ_c * (1 + λ / γ))) +
+         R_k_star / (R_k_star + 1 - δ) * r_k_f(t+1) +
+         (1 - δ) / (R_k_star + 1 - δ) * q_f(t+1) - q_f(t),
+         λ / γ / (1 + λ / γ) * c_f_m(t) +
+         1 / (1 + λ / γ) * c_f(t+1) +
+         (σ_c - 1) * w_hlc / (σ_c * (1 + λ / γ)) * (l_f(t) - l_f(t+1)) -
+         (1 - λ / γ) / (σ_c * (1 + λ / γ)) * r_f(t) + b(t) - c_f(t),
+         c_y * c_f(t) + i_y * i_f(t) + ε_g(t) + z_y * z_f(t) - y_f(t),
+         ϕ_p * (α * k_s_f(t) + (1 - α) * l_f(t) + ε_a(t)) - y_f(t),
+         σ_l * l_f(t) + 1 / (1 - λ / γ) * c_f(t) - λ / γ / (1 - λ / γ) * c_f_m(t) - w_f(t),
+         (1 - k_1) * k_f(t) + k_1 * i_f(t) + k_1 * γ^2 * φ * ε_i(t) - k_f(t+1),
          # Sticky Part
-         α * r_k + (1 - α) * w - ε_a - μ_pm, (1 - ψ) / ψ * r_k - z, w + l - k_s - r_k,
-         k + z - k_s, 1 / (1 + β * γ) * (i_m + β * γ * i_p + 1 / (γ^2 * φ) * q) + ε_i - i,
-         -r +
-         π_p +
-         b / ((1 - λ / γ) / (σ_c * (1 + λ / γ))) +
-         R_k_star / (R_k_star + 1 - δ) * r_k_p +
-         (1 - δ) / (R_k_star + 1 - δ) * q_p - q,
-         λ / γ / (1 + λ / γ) * c_m +
-         1 / (1 + λ / γ) * c_p +
-         (σ_c - 1) * w_hlc / (σ_c * (1 + λ / γ)) * (l - l_p) -
-         (1 - λ / γ) / (σ_c * (1 + λ / γ)) * (r - π_p) + b - c,
-         c_y * c + i_y * i + ε_g + z_y * z - y, ϕ_p * (α * k_s + (1 - α) * l + ε_a) - y,
-         1 / (1 + β * γ * ι_p) * (β * γ * π_p +
-                                  ι_p * π_m +
-                                  (1 - ξ_p) * (1 - β * γ * ι_p) / (ξ_p * ((ϕ_p - 1) * ε_p + 1)) * μ_pm) +
-         ε_pm - π,
-         1 / (1 + β * γ) * w_m + β * γ / (1 + β * γ) * w_p + ι_w / (1 + β * γ) * π_m -
-         (1 + β * γ * ι_w) / (1 + β * γ) * π +
-         β * γ / (1 + β * γ) * π_p +
+         α * r_k(t) + (1 - α) * w(t) - ε_a(t) - μ_pm(t), (1 - ψ) / ψ * r_k(t) - z(t),
+         w(t) + l(t) - k_s(t) - r_k(t), k(t) + z(t) - k_s(t),
+         1 / (1 + β * γ) * (i_m(t) + β * γ * i(t+1) + 1 / (γ^2 * φ) * q(t)) + ε_i(t) - i(t),
+         -r(t) + π(t+1) +
+         b(t) / ((1 - λ / γ) / (σ_c * (1 + λ / γ))) +
+         R_k_star / (R_k_star + 1 - δ) * r_k(t+1) +
+         (1 - δ) / (R_k_star + 1 - δ) * q(t+1) - q(t),
+         λ / γ / (1 + λ / γ) * c_m(t) +
+         1 / (1 + λ / γ) * c(t+1) +
+         (σ_c - 1) * w_hlc / (σ_c * (1 + λ / γ)) * (l(t) - l(t+1)) -
+         (1 - λ / γ) / (σ_c * (1 + λ / γ)) * (r(t) - π(t+1)) + b(t) - c(t),
+         c_y * c(t) + i_y * i(t) + ε_g(t) + z_y * z(t) - y(t),
+         ϕ_p * (α * k_s(t) + (1 - α) * l(t) + ε_a(t)) - y(t),
+         1 / (1 + β * γ * ι_p) * (β * γ * π(t+1) + ι_p * π_m(t) + (1 - ξ_p) * (1 - β * γ * ι_p) / (ξ_p * ((ϕ_p - 1) * ε_p + 1)) * μ_pm(t)) + ε_pm(t) - π(t),
+         1 / (1 + β * γ) * w_m(t) + β * γ / (1 + β * γ) * w(t+1) + ι_w / (1 + β * γ) * π_m(t) -
+         (1 + β * γ * ι_w) / (1 + β * γ) * π(t) +
+         β * γ / (1 + β * γ) * π(t+1) +
          (1 - ξ_w) * (1 - β * γ * ξ_w) / ((1 + β * γ) * ξ_w) / ((ϕ_w - 1) * ε_w + 1) *
-         (σ_l * l + 1 / (1 - λ / γ) * c - λ / γ / (1 - λ / γ) * c_m - w) +
-         ε_wm - w,
-         r_π * (1 - ρ) * π +
-         r_y * (1 - ρ) * (y - y_f) +
-         r_Δy * (y - y_f - y_m + y_f_m) +
-         ρ * r_m +
-         ε_r, (1 - k_1) * k + k_1 * i + k_1 * γ^2 * φ * ε_i - k_p,
+         (σ_l * l(t) + 1 / (1 - λ / γ) * c(t) - λ / γ / (1 - λ / γ) * c_m(t) - w(t)) + ε_wm(t) - w(t),
+         r_π * (1 - ρ) * π(t) + r_y * (1 - ρ) * (y(t) - y_f(t)) + r_Δy * (y(t) - y_f(t) - y_m(t) + y_f_m(t)) + ρ * r_m(t) + ε_r(t),
+         (1 - k_1) * k(t) + k_1 * i(t) + k_1 * γ^2 * φ * ε_i(t) - k(t+1),
          # Shock Transition
-         ρ_a * ε_a - ε_a_p, ρ_b * b - b_p, ρ_g * ε_g - ε_g_p, ρ_i * ε_i - ε_i_p,
-         ρ_r * ε_r - ε_r_p, ρ_p * ε_pm + η_p_aux_p - μ_p * η_p_aux - ε_pm_p, 0 - η_p_aux_p,
-         ρ_w * ε_wm + η_w_aux_p - μ_w * η_w_aux - ε_wm_p, 0 - η_w_aux_p,
+         ρ_a * ε_a(t) - ε_a(t+1), ρ_b * b(t) - b(t+1), ρ_g * ε_g(t) - ε_g(t+1), ρ_i * ε_i(t) - ε_i(t+1),
+         ρ_r * ε_r(t) - ε_r(t+1), ρ_p * ε_pm(t) + η_p_aux(t+1) - μ_p * η_p_aux(t) - ε_pm(t+1), 0 - η_p_aux(t+1),
+         ρ_w * ε_wm(t) + η_w_aux(t+1) - μ_w * η_w_aux(t) - ε_wm(t+1), 0 - η_w_aux(t+1),
          # Variable Mapping
-         y_f - y_f_m_p, y - y_m_p, c_f - c_f_m_p, c - c_m_p, i_f - i_f_m_p, i - i_m_p,
-         π - π_m_p, w - w_m_p, r - r_m_p]
+         y_f(t) - y_f_m(t+1), y(t) - y_m(t+1), c_f(t) - c_f_m(t+1), c(t) - c_m(t+1), i_f(t) - i_f_m(t+1), i(t) - i_m(t+1),
+         π(t) - π_m(t+1), w(t) - w_m(t+1), r(t) - r_m(t+1)]
 
-    x̄ = [y_f_m_ss ~ 0, y_m_ss ~ 0, k_f_ss ~ 0, k_ss ~ 0, c_f_m_ss ~ 0, c_m_ss ~ 0,
-          i_f_m_ss ~ 0, i_m_ss ~ 0, π_m_ss ~ 0, w_m_ss ~ 0, r_m_ss ~ 0, ε_a_ss ~ 0,
-          b_ss ~ 0, ε_g_ss ~ 0, ε_i_ss ~ 0, ε_r_ss ~ 0, ε_pm_ss ~ 0, η_p_aux_ss ~ 0,
-          ε_wm_ss ~ 0, η_w_aux_ss ~ 0]
-    ȳ = [k_s_f_ss ~ 0, k_s_ss ~ 0, r_f_ss ~ 0, r_ss ~ 0, r_k_f_ss ~ 0, r_k_ss ~ 0,
-          z_f_ss ~ 0, z_ss ~ 0, w_f_ss ~ 0, w_ss ~ 0, l_f_ss ~ 0, l_ss ~ 0, q_f_ss ~ 0,
-          q_ss ~ 0, y_f_ss ~ 0, y_ss ~ 0, i_f_ss ~ 0, i_ss ~ 0, c_f_ss ~ 0, c_ss ~ 0,
-          π_ss ~ 0, μ_pm_ss ~ 0]
-
-    x̄_iv = deepcopy(x̄)
-    ȳ_iv = deepcopy(ȳ)
+    steady_states = [y_f_m(∞) ~ 0, y_m(∞) ~ 0, k_f(∞) ~ 0, k(∞) ~ 0, c_f_m(∞) ~ 0, c_m(∞) ~ 0, i_f_m(∞) ~ 0, i_m(∞) ~ 0, π_m(∞) ~ 0, w_m(∞) ~ 0, r_m(∞) ~ 0, ε_a(∞) ~ 0, b(∞) ~ 0, ε_g(∞) ~ 0, ε_i(∞) ~ 0, ε_r(∞) ~ 0, ε_pm(∞) ~ 0, η_p_aux(∞) ~ 0, ε_wm(∞) ~ 0, η_w_aux(∞) ~ 0, k_s_f(∞) ~ 0, k_s(∞) ~ 0, r_f(∞) ~ 0, r(∞) ~ 0, r_k_f(∞) ~ 0, r_k(∞) ~ 0, z_f(∞) ~ 0, z(∞) ~ 0, w_f(∞) ~ 0, w(∞) ~ 0, l_f(∞) ~ 0, l(∞) ~ 0, q_f(∞) ~ 0, q(∞) ~ 0, y_f(∞) ~ 0, y(∞) ~ 0, i_f(∞) ~ 0, i(∞) ~ 0, c_f(∞) ~ 0, c(∞) ~ 0, π(∞) ~ 0, μ_pm(∞) ~ 0]
 
     n_ϵ = 2 # TODO: change
     n_x = length(x_sym)
@@ -143,5 +124,5 @@ function SW07()
     Γ[2, 2] = 1
     η = zeros(n_x, n_ϵ) # TODO: change
 
-    return H, (; x = x_sym, y = y_sym, x̄, ȳ, Γ, η, p_f, p, x̄_iv, ȳ_iv), "SW07"
+    return H, (; t, x = x_sym, y = y_sym, p, steady_states, Γ, η, p, simplify = false, simplify_Ψ = false), "SW07"
 end
