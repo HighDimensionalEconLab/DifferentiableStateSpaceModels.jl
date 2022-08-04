f157db6d89e9bef8e0cbe4df0145be0dad0de38c
Jesse Perla <jesseperla@gmail.com>
2022-08-04 11:10:50 -0700

    Add symbols to the sciml type for plotting (#149)

--- src/sequential.jl
+++ src/sequential.jl
@@ -4,18 +4,21 @@ function DifferenceEquations.LinearStateSpaceProblem(sol::AbstractFirstOrderPert
                                                      noise = nothing,
                                                      u0_prior_mean = zeros(size(sol.A, 1)),
                                                      u0_prior_var = sol.x_ergodic_var,
-                                                     observables_noise = sol.D)
+                                                     observables_noise = sol.D,
+                                                     syms = sol.u_symbols)
     return LinearStateSpaceProblem(sol.A, sol.B, x0, tspan; sol.C,
                                    observables_noise,
-                                   observables, noise, u0_prior_mean, u0_prior_var)
+                                   observables, noise, u0_prior_mean, u0_prior_var,
+                                   syms)
 end
 
 function DifferenceEquations.QuadraticStateSpaceProblem(sol::AbstractSecondOrderPerturbationSolution,
                                                         x0, tspan; observables = nothing,
                                                         observables_noise = sol.D,
-                                                        noise = nothing)
+                                                        noise = nothing,
+                                                        syms = sol.u_symbols)
     return QuadraticStateSpaceProblem(sol.A_0, sol.A_1, sol.A_2, sol.B, x0, tspan; sol.C_0,
                                       sol.C_1, sol.C_2,
                                       observables_noise,
-                                      observables, noise)
+                                      observables, noise, syms)
 end
